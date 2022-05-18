61eec6d21bf8a59a3d79afb97d22d409baef614e
kunalpratapsinghh <1.3kunal.tt@gmail.com>
2022-05-18 08:30:45 +0530

    navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+
+
+function Navbar() {
+  return (
+    <div className='container'>
+        <div>LOGOBAKERY</div>
+        <div className='middle'>
+            <div>Services</div>
+            <div>Project</div>
+            <div>About</div>
+        </div>
+        <div>
+            <button>Contact</button>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
